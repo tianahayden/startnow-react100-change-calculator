45b9a41393dc7e1e6f002d00fde61eeb40fafd00
Tiana Hayden <tiana.hayden@me.com>
2018-09-26 11:51:53 -0700

    Added MXN as currency option. Created MXN calculation function. MXN table renders when state of dropdown is MXN.

--- src/app.jsx
+++ src/app.jsx
@@ -107,8 +107,111 @@ class Table extends Component {
     }
 
 
+    if (this.props.selectedCurrency === 'MXN') {
 
+      const divStyle = {
+        minHeight: '120px',
+      };
+
+      return (
+        <div className='col-sm-6'>
+          <div className='panel panel-default'>
+            <div className='panel-body'>
+              <div className='alert alert-success'>{this.props.outputMessage}{this.props.totalChangeDue}</div>
+
+              <div className='row'>
+                <div className='col-sm-3'>
+                  <div className='panel panel-default'>
+                    <div className='panel-body text-center' style={divStyle}>
+                      Five Hundreds
+              <h4 className='change'>
+                        {this.props.MXN500d}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-3'>
+                  <div className='panel panel-default'>
+                    <div className='panel-body text-center' style={divStyle}>
+                      Two Hundreds
+              <h4 className='change'>
+                        {this.props.MXN200d}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-3'>
+                  <div className='panel panel-default'>
+                    <div className='panel-body text-center' style={divStyle}>
+                      One Hundreds
+              <h4 className='change'>
+                        {this.props.MXN100d}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-3'>
+                  <div className='panel panel-default'>
+                    <div className='panel-body text-center' style={divStyle}>
+                      Fifties
+              <h4 className='change'>
+                        {this.props.MXN50d}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='row'>
+                <div className='col-sm-3'>
+                  <div className='panel panel-default'>
+                    <div className='panel-body text-center' style={divStyle}>
+                      Tens
+              <h4 className='change'>
+                        {this.props.MXN10d}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-3'>
+                  <div className='panel panel-default'>
+                    <div className='panel-body text-center' style={divStyle}>
+                      Ones
+              <h4 className='change'>
+                        {this.props.MXN1d}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-3'>
+                  <div className='panel panel-default'>
+                    <div className='panel-body text-center' style={divStyle}>
+                      Fifty Cents
+              <h4 className='change'>
+                        {this.props.MXN50c}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-3'>
+                  <div className='panel panel-default'>
+                    <div className='panel-body text-center' style={divStyle}>
+                      Ten Cents
+              <h4 className='change'>
+                        {this.props.MXN10c}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
+
+
 
 }
 
@@ -135,13 +238,26 @@ class App extends Component {
       pennies: '',
       outputMessage: '',
       isToggle: false,
-      selectedCurrency: 'USD'
+      selectedCurrency: 'USD',
+      billsMXN: '',
+      coinsMXN: '',
+      billsOutputMXN: '',
+      coinsOutputMXN: '',
+      MXN500d: '',
+      MXN200d: '',
+      MXN100d: '',
+      MXN50d: '',
+      MXN10d: '',
+      MXN1d: '',
+      MXN50c: '',
+      MXN10c: '',
     };
 
     this.handleDueChange = this.handleDueChange.bind(this);
     this.handleRecievedChange = this.handleRecievedChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.calculateChange = this.calculateChange.bind(this);
+    this.calculateUSDChange = this.calculateUSDChange.bind(this);
+    this.calculateMXNChange = this.calculateMXNChange.bind(this);
     this.handleSelectChange = this.handleSelectChange.bind(this);
 
   };
@@ -167,6 +283,18 @@ class App extends Component {
         outputMessage={this.state.outputMessage}
         isToggle={this.state.isToggle}
         selectedCurrency={this.state.selectedCurrency}
+        billsMXN={this.state.billsMXN}
+        coinsMXN={this.state.coinsMXN}
+        billsOutputMXN={this.state.billsOutputMXN}
+        coinsOutputMXN={this.state.coinsOutputMXN}
+        MXN500d={this.state.MXN500d}
+        MXN200d={this.state.MXN200d}
+        MXN100d={this.state.MXN100d}
+        MXN50d={this.state.MXN50d}
+        MXN10d={this.state.MXN10d}
+        MXN1d={this.state.MXN1d}
+        MXN50c={this.state.MXN50c}
+        MXN10c={this.state.MXN10c}
       />
     )
   }
@@ -184,7 +312,14 @@ class App extends Component {
   }
 
   handleClick() {
-    this.calculateChange();
+    if (this.state.selectedCurrency === 'USD') {
+      this.calculateUSDChange();
+    }
+
+    if (this.state.selectedCurrency === 'MXN') {
+      this.calculateMXNChange();
+    }
+
 
     this.setState({
       isToggle: true
@@ -197,7 +332,7 @@ class App extends Component {
     });
   };
 
-  calculateChange() {
+  calculateUSDChange() {
     var totalChangeDue = (parseFloat(this.state.recievedValue) - parseFloat(this.state.dueValue)).toFixed(2);
     var dollars = totalChangeDue.toString().slice(0, -3);
     var coins = totalChangeDue.toString().slice(-2)
@@ -253,10 +388,68 @@ class App extends Component {
       dimes: coinsOutput[1],
       nickles: coinsOutput[2],
       pennies: coinsOutput[3],
-      outputMessage: 'Total change due: $'
+      outputMessage: 'Total change due: USD $'
+    })
+  }
+
+  calculateMXNChange() {
+    var totalChangeDue = (parseFloat(this.state.recievedValue) - parseFloat(this.state.dueValue)).toFixed(2);
+    var bills = totalChangeDue.toString().slice(0, -3);
+    var coins = totalChangeDue.toString().slice(-2)
+
+    // sets the state of total change, broken into bills and coins
+    this.setState({
+      totalChangeDue: totalChangeDue,
+      billsMXN: bills,
+      coinsMXN: coins,
     })
 
+    // calculates how much to give in each bill type
+    var billsArr = [500, 200, 100, 50, 10, 1]
+    var billsOutput = []
+    Number(bills)
 
+    for (let i = 0; i < billsArr.length; i++) {
+      if (bills > 0) {
+        var ans = Math.floor(bills / billsArr[i])
+        billsOutput.push(ans)
+        bills = bills - (billsArr[i] * ans)
+      }
+      else {
+        billsOutput.push(0)
+      }
+    }
+
+    // calculates how much change to give in each coin type
+    var coinsArr = [50, 10]
+    var coinsOutput = []
+    Number(coins)
+
+    for (let i = 0; i < coinsArr.length; i++) {
+      if (coins > 0) {
+        var ans = Math.floor(coins / coinsArr[i])
+        coinsOutput.push(ans)
+        coins = coins - (coinsArr[i] * ans)
+      }
+      else {
+        coinsOutput.push(0)
+      }
+    }
+
+    // sets the state to be the correct change needed
+    this.setState({
+      billsOutputMXN: billsOutput,
+      MXN500d: billsOutput[0],
+      MXN200d: billsOutput[1],
+      MXN100d: billsOutput[2],
+      MXN50d: billsOutput[3],
+      coinsOutputMXN: coinsOutput,
+      MXN10d: billsOutput[4],
+      MXN1d: billsOutput[5],
+      MXN50c: coinsOutput[0],
+      MXN10c: coinsOutput[1],
+      outputMessage: 'Total change due: MXN $'
+    })
   }
 
 
@@ -288,7 +481,7 @@ class App extends Component {
                 <label>What Currency?</label>
                 <select onChange={this.handleSelectChange} className="form-control">
                   <option value="USD">USD</option>
-                  <option value="EUR">EUR</option>
+                  <option value="MXN">MXN</option>
                 </select>
               </div>
               <div className='panel-footer'>
@@ -308,3 +501,4 @@ class App extends Component {
 }
 
 export default App;
+
